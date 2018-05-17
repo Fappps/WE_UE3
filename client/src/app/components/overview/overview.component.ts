5a68e0536d50c89e1ddeb79d6abb5ad655ee1b73
Stanislav Vasic <s.vasic@durchblicker.at>
2018-05-17 10:52:30 +0200

    added available first iteration

--- client/src/app/components/overview/overview.component.ts
+++ client/src/app/components/overview/overview.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { AvailableDevice } from '../../models/device.available';
+import { ControlType } from '../../models';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html'
 })
 export class OverviewComponent implements OnInit {
-
+  devices: AvailableDevice[]=[];
   constructor(private router: Router) { }
 
   ngOnInit() {
-    
+    this.devices.push({
+      "title": "3D-Drucker",
+      "type": "item-generator",
+      "image": "images/item_generator.svg",
+      "control": {
+        "title": "Produktfortschritt einstellen",
+        "type": ControlType.ENUM,
+        "current": "Teilweise fertig",
+        "log":[]
+      }
+    });
   }
 
   logout(){
